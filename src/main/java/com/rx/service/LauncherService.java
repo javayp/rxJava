3f53952455cafadf63660487ec0ac049ec3f6896
Prashanth YP <javayp@users.noreply.github.com>
2018-10-01 16:50:30 +0530

    Update LauncherService.java

--- src/main/java/com/rx/service/LauncherService.java
+++ src/main/java/com/rx/service/LauncherService.java
@@ -1,5 +1,7 @@
 package com.rx.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.stereotype.Service;
 
 import io.reactivex.Observable;
@@ -10,5 +12,11 @@ public class LauncherService {
 	public void nameService() {
 		Observable<String> myStrings=Observable.just("Alpha", "beta", "gamma");
 		myStrings.subscribe(ele->System.out.println(ele));
+		
+		myStrings.map(ele->ele.length()).subscribe(len->System.out.println("len="+len));
+		
+		
+		Observable<Long> secondsInterval=Observable.interval(1, TimeUnit.SECONDS);
+		secondsInterval.subscribe();
 	}
 }
